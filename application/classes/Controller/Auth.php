182f9ca97a48793c616519f1e3d1b0063a177c5a
Oded Arbel <oded@geek.co.il>
2015-09-04 07:51:52 -0700

    Created

---
+++ application/classes/Controller/Auth.php
@@ -0,0 +1,22 @@
+<?php
+
+class Controller_Auth extends Controller {
+
+  public function action_verify() {
+    $data = json_decode($this->request->body());
+    if ($data['token'] && $this->is_valid($data['token']))
+      $this->send([ "status" => true ]);
+    else
+      $this->send([ "status" => false ];)
+  }
+  
+  public function action_start() {
+    $data = json_decode($this->request->body());
+    $this->send([ "auth-url" => Auth::getProvider($data['provider'] || 'google')->startAuthentication() ]);
+  }
+  
+  private function is_valid($token) {
+    return false;
+  }
+
+}
