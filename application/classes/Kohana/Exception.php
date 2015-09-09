eaeac08b91b10d6aa02c6fa212a6c46a61d368ad
Oded Arbel <oded@geek.co.il>
2015-09-09 21:47:08 +0300

    handle PHP 7 errors gracefully (with just a bit of interface mismatch wrning)

--- application/classes/Kohana/Exception.php
+++ application/classes/Kohana/Exception.php
@@ -4,9 +4,11 @@ class Kohana_Exception extends Kohana_Kohana_Exception {
 	
 	public static function handler($e)
 	{
-		if ($e instanceof Exception) {
+		if ($e instanceof Exception)
 			return Kohana_Kohana_Exception::handler($e);
-		} else {
+		elseif ($e instanceof Error)
+			return Kohana_Kohana_Exception::handler(new ErrorException($e->getMessage(), $e->getCode(), 1, $e->getFile(), $e->getLine(), $e->getPrevious()));
+		else {
 			var_dump($e);
 			return Kohana_Kohana_Exception::handler(new Exception("Invalid type sent to handler(): '".get_class($e)."'"));
 		}
