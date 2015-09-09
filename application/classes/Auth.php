da2290dabcba04673a45a8ab6a06bc4b24d8d3dc
Oded Arbel <oded@geek.co.il>
2015-09-10 00:42:02 +0300

    allow retrieving last used provider from session

--- application/classes/Auth.php
+++ application/classes/Auth.php
@@ -2,13 +2,23 @@
 
 class Auth {
 	
+	const USER_AUTH_PROVIDER = 'user-auth-provider';
+	
 	public static function getProvider($provider, $callback_url) {
 		$config = static::getConfig();
 		if (!$config[$provider])
 			throw new Exception("Invalid provider specified: #{$provider}");
 		$prov_config = $config[$provider];
 		$fullclass = "Auth_" . str_replace('/', '_', $prov_config['type']);
-		return new $fullclass($prov_config, $callback_url);
+		$provider = new $fullclass($prov_config, $callback_url);
+		Session::instance()->bind(self::USER_AUTH_PROVIDER, $provider);
+		return $provider;
+	}
+	
+	public static function getLastProvider() {
+		$provider = Session::instance()->get(self::USER_AUTH_PROVIDER);
+		if (is_null($provider)) throw new Exception("Failed to find current auth provider");
+		return $provider;
 	}
 	
 	private static function getConfig() {
