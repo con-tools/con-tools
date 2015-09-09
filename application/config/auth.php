f6f09c0a03fc9275c1ee055a00e8940db4f4615f
Oded Arbel <oded@geek.co.il>
2015-09-09 22:52:13 +0300

    fix oauth endpoint

--- application/config/auth.php
+++ application/config/auth.php
@@ -7,6 +7,7 @@ return [
 				'type'		=> 'OpenIDConnect',
 				'id'		=> $auth_token[0],
 				'secret'	=> $auth_token[1],
-				'endpoint'	=> 'https://accounts.google.com/o/oauth2/auth',
+//				'endpoint'	=> 'https://accounts.google.com/o/oauth2/auth',
+				'endpoint'	=> 'https://accounts.google.com',
 		],
 ];
