fc434f4e50c4c8aeb0872f866f8de1a035c65e84
Oded Arbel <oded@geek.co.il>
2015-09-11 01:49:32 +0300

    add login method that update time stamps and generates a token

--- application/classes/Model/User.php
+++ application/classes/Model/User.php
@@ -1,8 +1,14 @@
 <?php
+
 class Model_User extends ORM {
 	
-	protected $_fields = [
-			'login_time' => [ 'type' => 'DateTime' ]
+	protected $_columns = [
+			'created_time' => [ 'type' => 'DateTime' ],
+			'login_time' => [ 'type' => 'DateTime' ],
+	];
+	
+	protected $_has_many = [
+			'tokens' => [],
 	];
 
 	public function rules() {
@@ -17,14 +23,36 @@ class Model_User extends ORM {
 		];
 	}
 	
-	public static function persist($name, $email, $provider) {
+	/**
+	 * Perform a login by retrieving or generating a relevant login token
+	 * and updating all relevant time fields 
+	 * @param string $type type of token to use. One of 'web', 'api'
+	 */
+	public function login($type = 'web') {
+		$token = $this->getValidToken($type);
+		$token->touch();
+		$this->login_time = new DateTime();
+		return $token;
+	}
+	
+	private function getValidToken($type) {
+		foreach ($this->tokens->where('type', '=', $type)->find_all() as $token) {
+			if (!$token->is_expired())
+				return $token;
+		}
+		
+		return Model_Token::presist($this, $type, Time_Unit::weeks(2));
+	}
+	
+	public static function persist($name, $email, $provider, $token) {
 		$o = new Model_User();
 		$o->name = $name;
 		$o->email = $email;
 		$o->provider = $provider;
+		$o->password = $token;
 		$o->login_time = new DateTime();
 		$o->save();
 		return $o;
 	}
-
+	
 }
