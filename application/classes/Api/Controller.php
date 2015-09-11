8d00e1b589dd402815642b35067476fdbad01965
Oded Arbel <oded@geek.co.il>
2015-09-11 03:05:24 +0300

    add verify method to handle user authorization

--- application/classes/Api/Controller.php
+++ application/classes/Api/Controller.php
@@ -3,6 +3,27 @@ abstract class Api_Controller extends Controller {
 
 	public $auto_render = false;
 
+	/**
+	 * Call from API controllers to verify authorization on calls that require user authorization
+	 * @throws HTTP_Exception_403 in case authorization was denied
+	 */
+	protected function verifyAuthentication() {
+		$auth = $this->request->headers('Authorization');
+		if (!$auth)
+			throw new HTTP_Exception_403("No Authorization header present");
+		try {
+			$token = Model_Token::byToken($auth);
+			if ($token->is_expired())
+				throw new HTTP_Exception_403("Authorization token expired");
+		} catch (Model_Exception_NotFound $e) {
+			throw new HTTP_Exception_403("Invalid Authorization header");
+		}
+	}
+	
+	/**
+	 * Send a response to the caller in JSON format
+	 * @param mixed $data Data to send
+	 */
 	protected function send($data) {
 		$this->response->headers('Content-Type', 'application/json');
 		$this->response->body(json_encode($data));
