a38d8fa1319c332611e21a0c6d5a25bb2e0c8d09
Oded Arbel <oded@geek.co.il>
2015-09-14 21:33:33 +0300

    configure crud route

---
+++ application/config/routes.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+	
+	'crud' => [
+		'uri' => 'entities/<controller>(/<id>)',
+		'defaults' => [
+			'directory' => 'Entities',
+			'action' => 'index',
+		],
+	],
+		
+	'default' => [
+		'uri' => '(<controller>(/<action>(/<id>)))',
+		'defaults' => [
+			'controller' => 'welcome',
+			'action'     => 'index',
+		],
+	],
+
+];
