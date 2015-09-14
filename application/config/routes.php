0a0bb57fc4a43551824c0ecb0be97d4f2d7823cd
Oded Arbel <oded@geek.co.il>
2015-09-14 22:13:15 +0300

    debug

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,20 +1,20 @@
 <?php
 
 return [
-	
-	[ 'crud' => [
-		'uri' => 'entities/<controller>(/<id>)',
-		'defaults' => [
-			'directory' => 'Entities',
-			'action' => 'index',
-		],
-	]],
 		
 	['default' => [
 		'uri' => '(<controller>(/<action>(/<id>)))',
 		'defaults' => [
 			'controller' => 'welcome',
 			'action'     => 'index',
+		],
+	]],
+	
+	[ 'crud' => [
+		'uri' => 'entities/<controller>(/<id>)',
+		'defaults' => [
+			'directory' => 'Entities',
+			'action' => 'index',
 		],
 	]],
 
