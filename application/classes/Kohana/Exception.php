e175d297f86c0032fab78359f6fc72945262deed
Oded Arbel <oded@geek.co.il>
2015-09-14 16:15:56 +0300

    debug

--- application/classes/Kohana/Exception.php
+++ application/classes/Kohana/Exception.php
@@ -4,6 +4,7 @@ class Kohana_Exception extends Kohana_Kohana_Exception {
 	
 	public static function handler($e)
 	{
+		error_log("Error: $e");
 		if ($e instanceof Exception)
 			return Kohana_Kohana_Exception::handler($e);
 		elseif ($e instanceof Error)
