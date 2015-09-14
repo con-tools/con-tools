9dd5a01d2b3284b78026ba181b3000375b1f0395
Oded Arbel <oded@geek.co.il>
2015-09-14 20:54:16 +0300

    add persist to user record

--- application/classes/Model/User/Record.php
+++ application/classes/Model/User/Record.php
@@ -10,6 +10,17 @@ class Model_User_Record extends ORM {
 			'user' => [],
 			'convention' => [],
 	];
+	
+	public static function persist(Model_Convention $con, Model_User $user, $descriptor, $content_type, $data) {
+		$o = new Model_User_Record();
+		$o->convention = $con;
+		$o->user = $user;
+		$o->descriptor = $descriptor;
+		$o->content_type = $content_type;
+		$o->data = $data;
+		$o->save;
+		return $o;
+	}
 
 	public static function byDescriptor(Model_Convention $con, Model_User $user, $descriptor) {
 		Model::factory('user_record')
