08e8b2fdd5e0bf4c5a7f9ef0073bb1099481ef53
Oded Arbel <oded@geek.co.il>
2015-09-15 23:50:41 +0300

    add facebook auth implementation based on leagues provider

---
+++ application/classes/Auth/LeagueOAuth2.php
@@ -0,0 +1,110 @@
+<?php
+class Auth_LeagueOAuth2 implements Auth_ProviderIf {
+
+	/**
+	 * Application's OAuth client ID
+	 * 
+	 * @var String $client_id
+	 */
+	private $client_id = null;
+
+	/**
+	 * Application's OAuth client secret
+	 * 
+	 * @var String $secret
+	 */
+	private $secret = null;
+
+	private $name = null;
+
+	private $provider = null;
+	
+	/**
+	 * retrieved authentication token
+	 */
+	private $token = null;
+
+	/**
+	 * retrieved authenticated user
+	 */
+	private $user = null;
+
+	public function __construct($name, $configuration, $callback_url) {
+		$this->client_id = $configuration['id'];
+		$this->secret = $configuration['secret'];
+		$this->name = $name;
+		$provider_name = $configuration['provider'] ?  : 'GenericProvider';
+		$full_provider_name = "League\\OAuth2\\Client\\Provider\\{$provider_name}";
+		$opts = array_merge([
+				'clientId' => $this->client_id,
+				'clientSecret' => $this->secret,
+				'redirectUri' => $callback_url 
+		], $configuration['config'] ?  : []);
+		$this->provider = new $full_provider_name($opts);
+	}
+
+	/*
+	 * (non-PHPdoc)
+	 * @see Auth_ProviderIf::getAuthenticationURL()
+	 */
+	public function getAuthenticationURL($redirect_url) {
+		$authUrl = $this->provider->getAuthorizationUrl([
+				'scope' => [ 'email', 'name' ]
+		]);
+		Session::instance()->set('oauth2state', $this->provider->getState());
+		Session::instance()->set('oauth2-callback-url', $redirect_url);
+		return $authUrl;
+	}
+
+	/*
+	 * (non-PHPdoc)
+	 * @see Auth_ProviderIf::complete()
+	 */
+	public function complete($code, $state) {
+		if ($state != Session::instance()->get('oauth2state'))
+			throw new Exception("Invalid authorization state!");
+		$this->token = $this->provider->getAccessToken('authorization_code', [ 'code' => $code ]);
+		$this->user = $this->provider->getResourceOwner($this->token);
+	}
+
+	/*
+	 * (non-PHPdoc)
+	 * @see Auth_ProviderIf::getName()
+	 */
+	public function getName() {
+		return $this->user->getName();
+	}
+
+	/*
+	 * (non-PHPdoc)
+	 * @see Auth_ProviderIf::getEmail()
+	 */
+	public function getEmail() {
+		return $this->user->getEmail();
+	}
+
+	/*
+	 * (non-PHPdoc)
+	 * @see Auth_ProviderIf::getProviderName()
+	 */
+	public function getProviderName() {
+		return $this->name;
+	}
+
+	/*
+	 * (non-PHPdoc)
+	 * @see Auth_ProviderIf::getToken()
+	 */
+	public function getToken() {
+		$this->token->getToken();
+	}
+
+	/*
+	 * (non-PHPdoc)
+	 * @see Auth_ProviderIf::getRedirectURL()
+	 */
+	public function getRedirectURL() {
+		return Session::instance()->get('oauth2-callback-url');
+	}
+
+}
--- application/config/auth.php
+++ application/config/auth.php
@@ -1,13 +1,26 @@
 <?php
 
-$auth_token = explode(':', $_SERVER['GOOGLE_AUTH_CLIENT']);
+$google_auth_token = explode(':', $_SERVER['GOOGLE_AUTH_CLIENT']);
+$facebook_auth_token = explode(':', $_SERVER['FACEBOOK_AUTH_CLIENT']);
 
 return [
 		'google' => [
+				'name'		=> 'Google',
+				'icon'		=> 'http://api.con-troll.org/images/auth/google/btn_google_signin_dark_normal_web.png',
 				'type'		=> 'OpenIDConnect',
-				'id'		=> $auth_token[0],
-				'secret'	=> $auth_token[1],
+				'id'		=> $google_auth_token[0],
+				'secret'	=> $google_auth_token[1],
 //				'endpoint'	=> 'https://accounts.google.com/o/oauth2/auth',
 				'endpoint'	=> 'https://accounts.google.com',
 		],
+		
+		'facebook' => [
+				'name'		=> 'Facebook',
+				'icon'		=> 'http://api.con-troll.org/images/auth/facebook/facebook-login-with.png',
+				'type'		=> 'LeagueOAuth2',
+				'id'		=> $facebook_auth_token[0],
+				'secret'	=> $facebook_auth_token[1],
+				'provider'	=> 'Facebook',
+				'config'	=> [ 'graphApiVersion' => 'v2.4' ],
+		]
 ];
