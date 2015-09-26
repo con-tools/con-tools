4072bbfa474f6e3b84f8af41d3f8c52de2322a12
Oded Arbel <oded@geek.co.il>
2015-09-26 17:47:10 +0300

    add twitter ad openid connection provider. This may not work.

--- application/config/auth.php
+++ application/config/auth.php
@@ -1,6 +1,7 @@
 <?php
 
 $google_auth_token = explode(':', $_SERVER['GOOGLE_AUTH_CLIENT']);
+$twitter_auth_token = explode(':', $_SERVER['TWITTER_AUTH_CLIENT']);
 $facebook_auth_token = explode(':', $_SERVER['FACEBOOK_AUTH_CLIENT']);
 
 return [
@@ -12,6 +13,15 @@ return [
 				'secret'	=> $google_auth_token[1],
 //				'endpoint'	=> 'https://accounts.google.com/o/oauth2/auth',
 				'endpoint'	=> 'https://accounts.google.com',
+		],
+		
+		'twitter' => [
+				'name'		=> 'Twitter',
+				'button'		=> 'http://api.con-troll.org/images/auth/google/btn_google_signin_dark_normal_web.png',
+				'type'		=> 'OpenIDConnect',
+				'id'		=> $twitter_auth_token[0],
+				'secret'	=> $twitter_auth_token[1],
+				'endpoint'	=> 'https://api.twitter.com',
 		],
 		
 		'facebook' => [
