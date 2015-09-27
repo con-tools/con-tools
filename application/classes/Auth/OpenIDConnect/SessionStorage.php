489f98d834a041a577be79189e3f9a0616b1da59
Oded Arbel <oded@geek.co.il>
2015-09-27 13:43:56 +0300

    add openid connection session store driver implementing Kohana session storage

---
+++ application/classes/Auth/OpenIDConnect/SessionStorage.php
@@ -0,0 +1,46 @@
+<?php
+
+class Auth_OpenIDConnection_SessionStorage implements OpenIDSessionStorageIF {
+	
+	/**
+	 * {@inheritDoc}
+	 * @see OpenIDSessionStorageIF::storeNonce()
+	 */
+	public function storeNonce($nonce) {
+		Session::instance()->set('auth-openid-connect-nonce', $nonce);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see OpenIDSessionStorageIF::getNonce()
+	 */
+	public function getNonce() {
+		Session::instance()->get('auth-openid-connect-nonce', null);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see OpenIDSessionStorageIF::storeState()
+	 */
+	public function storeState($state) {
+		Session::instance()->set('auth-openid-connect-state', $state);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see OpenIDSessionStorageIF::getState()
+	 */
+	public function getState() {
+		Session::instance()->get('auth-openid-connect-state', null);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 * @see OpenIDSessionStorageIF::clear()
+	 */
+	public function clear() {
+		Session::instance()->delete('auth-openid-connect-state');
+		Session::instance()->delete('auth-openid-connect-nonce');
+	}
+
+}
