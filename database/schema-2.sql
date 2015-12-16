39f0aaf5479b95acd27cabb9d1a124abc46beb5b
Oded Arbel <oded@geek.co.il>
2015-12-16 22:57:49 +0200

    added event stuff

--- database/schema-2.sql
+++ database/schema-2.sql
@@ -19,6 +19,55 @@ CREATE TABLE `roles` (
   PRIMARY KEY (`id`)
 ) ENGINE=INNODB DEFAULT CHARACTER SET UTF8;
 
+CREATE TABLE `events` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '',
+  `user_id` INT UNSIGNED NOT NULL COMMENT '',
+  `staff_contact_id` INT UNSIGNED NOT NULL COMMENT '',
+  `convention_id` INT UNSIGNED NOT NULL COMMENT '',
+  `title` VARCHAR(255) NOT NULL COMMENT '',
+  `teaser` TEXT NOT NULL COMMENT '',
+  `description` TEXT NOT NULL COMMENT '',
+  `price` DECIMAL(5,2) NOT NULL COMMENT '',
+  `requires_registration` BOOLEAN DEFAULT TRUE COMMENT '',
+  `duration` INT NOT NULL COMMENT 'in seconds to make epoch calculations easier'
+  `min_attendees` INT NOT NULL DEFAULT 0 COMMENT '',
+  `max_attendees` INT DEFAULT NULL COMMENT 'null value means "open event"',
+  `notes_to_staff` TEXT NOT NULL DEFAULT '' COMMENT '',
+  `notes_to_attendees` TEXT NOT NULL DEFAULT '' COMMENT '',
+  `scheduling_constraints` TEXT NOT NULL DEFAULT '' COMMENT '',
+  PRIMARY KEY (`id`),
+  CONSTRAINT `events_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `events_ibfk_2` FOREIGN KEY (`staff_contact_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `events_ibfk_3` FOREIGN KEY (`convention_id`) REFERENCES `conventions` (`id`) ON DELETE CASCADE
+) ENGINE=INNODB DEFAULT CHARACTER SET UTF8;
+
+CREATE TABLE `event_tag_types` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '',
+  `convention_id` INT UNSIGNED NOT NULL COMMENT '',
+  `title` VARCHAR(50) NOT NULL COMMENT '',
+  `requirement`
+  `visible` BOOLEAN NOT NULL DEFAULT TRUE COMMENT '',
+  PRIMARY KEY (`id`),
+  CONSTRAINT `event_tag_types_ibfk_1` FOREIGN KEY (`convention_id`) REFERENCES `conventions` (`id`) ON DELETE CASCADE
+) ENGINE=INNODB DEFAULT CHARACTER SET UTF8;
+
+CREATE TABLE `event_tag_values` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '',
+  `event_tag_type_id` INT UNSIGNED NOT NULL COMMENT '',
+  `title` VARCHAR(50) NOT NULL COMMENT '',
+  PRIMARY KEY (`id`),
+  CONSTRAINT `event_tag_values_ibfk_1` FOREIGN KEY (`event_tag_type_id`) REFERENCES `event_tag_types` (`id`) ON DELETE CASCADE
+) ENGINE=INNODB DEFAULT CHARACTER SET UTF8;
+
+CREATE TABLE `event_tags` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '',
+  `event_id` INT UNSIGNED NOT NULL COMMENT '',
+  `event_tag_value_id` INT UNSIGNED NOT NULL COMMENT '',
+  PRIMARY KEY (`id`),
+  CONSTRAINT `event_tags_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `event_tags_ibfk_2` FOREIGN KEY (`event_tag_value_id`) REFERENCES `event_tag_values` (`id`) ON DELETE CASCADE
+) ENGINE=INNODB DEFAULT CHARACTER SET UTF8;
+
 CREATE TABLE `crm_queues` (
   `id` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '',
   `convention_id` INT UNSIGNED NOT NULL COMMENT '',
@@ -33,7 +82,7 @@ CREATE TABLE `crm_issues` (
   `crm_queue_id` INT UNSIGNED NOT NULL COMMENT '',
   `agent_id` INT UNSIGNED NOT NULL COMMENT '',
   `title` TEXT NOT NULL COMMENT '',
-  `status` VARCHAR(30) NOT NULL COMMENT '',
+  `status` VARCHAR(30) NOT NULL DEFAULT 'unassigned' COMMENT 'One of: unassigned, open, awaiting-approval, ready-for-timeslotting, closed',
   PRIMARY KEY (`id`),
   CONSTRAINT `crm_issues_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`) ON DELETE CASCADE,
   CONSTRAINT `crm_issues_ibfk_2` FOREIGN KEY (`crm_queue_id`) REFERENCES `crm_queue` (`id`) ON DELETE CASCADE,
@@ -52,6 +101,19 @@ CREATE TABLE `crm_messages` (
   CONSTRAINT `crm_messages_ibfk_1` FOREIGN KEY (`crm_issue_id`) REFERENCES `crm_issues` (`id`) ON DELETE CASCADE,
   CONSTRAINT `crm_messages_ibfk_2` FOREIGN KEY (`sender_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
   CONSTRAINT `crm_messages_ibfk_3` FOREIGN KEY (`in_reply_to`) REFERENCES `crm_messages` (`id`) ON DELETE CASCADE
+) ENGINE=INNODB DEFAULT CHARACTER SET UTF8;
+
+CREATE TABLE `coupon_types` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '',
+  `convention_id` INT UNSIGNED NOT NULL COMMENT '',
+  `title` VARCHAR(255) NOT NULL COMMENT '',
+  `discount_type` VARCHAR(8) NOT NULL DEFAULT 'fixed' COMMENT 'One of: fixed, percent',
+  `amount` DECIMAL(5,2) NOT NULL DEFAULT 0 COMMENT '',
+  `category` VARCHAR(255) NOT NULL COMMENT '',
+  `multiuse` BOOLEAN NOT NULL DEFAULT FALSE COMMENT '',
+  `code` VARCHAR(255) DEFAULT NULL COMMENT 'coupon code to type in or scan',
+  PRIMARY KEY (`id`),
+  CONSTRAINT `coupon_types_ibfk_1` FOREIGN KEY (`convention_id`) REFERENCES `conventions` (`id`) ON DELETE CASCADE
 ) ENGINE=INNODB DEFAULT CHARACTER SET UTF8;
 
 UPDATE `system_settings` SET `value` = '2' WHERE `name` = 'data-version';
