d293240f0c5798591e0b22039468f1d9bb589f45
Oded Arbel <oded@geek.co.il>
2015-12-16 22:36:04 +0200

    added CRM stuff

--- database/schema-2.sql
+++ database/schema-2.sql
@@ -18,5 +18,40 @@ CREATE TABLE `roles` (
   `title` VARCHAR(20) NOT NULL COMMENT '',
   PRIMARY KEY (`id`)
 ) ENGINE=INNODB DEFAULT CHARACTER SET UTF8;
-	
+
+CREATE TABLE `crm_queues` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '',
+  `convention_id` INT UNSIGNED NOT NULL COMMENT '',
+  `title` VARCHAR(255) NOT NULL COMMENT '',
+  PRIMARY KEY (`id`),
+  CONSTRAINT `crm_queues_ibfk_1` FOREIGN KEY (`convention_id`) REFERENCES `conventions` (`id`) ON DELETE CASCADE
+) ENGINE=INNODB DEFAULT CHARACTER SET UTF8;
+
+CREATE TABLE `crm_issues` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '',
+  `event_id` INT UNSIGNED NOT NULL COMMENT '',
+  `crm_queue_id` INT UNSIGNED NOT NULL COMMENT '',
+  `agent_id` INT UNSIGNED NOT NULL COMMENT '',
+  `title` TEXT NOT NULL COMMENT '',
+  `status` VARCHAR(30) NOT NULL COMMENT '',
+  PRIMARY KEY (`id`),
+  CONSTRAINT `crm_issues_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `crm_issues_ibfk_2` FOREIGN KEY (`crm_queue_id`) REFERENCES `crm_queue` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `crm_issues_ibfk_3` FOREIGN KEY (`agent_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=INNODB DEFAULT CHARACTER SET UTF8;
+
+CREATE TABLE `crm_messages` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '',
+  `crm_issue_id` INT UNSIGNED NOT NULL COMMENT '',
+  `sender_id` INT UNSIGNED NOT NULL '',
+  `subject` TEXT NOT NULL COMMENT '',
+  `text` TEXT NOT NULL COMMENT '',
+  `in_reply_to` INT UNSIGNED DEFAULT NULL COMMENT '',
+  `smtp_message_id` VARCHAR(255) DEFAULT NULL COMMENT '',
+  PRIMARY KEY (`id`),
+  CONSTRAINT `crm_messages_ibfk_1` FOREIGN KEY (`crm_issue_id`) REFERENCES `crm_issues` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `crm_messages_ibfk_2` FOREIGN KEY (`sender_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `crm_messages_ibfk_3` FOREIGN KEY (`in_reply_to`) REFERENCES `crm_messages` (`id`) ON DELETE CASCADE
+) ENGINE=INNODB DEFAULT CHARACTER SET UTF8;
+
 UPDATE `system_settings` SET `value` = '2' WHERE `name` = 'data-version';
