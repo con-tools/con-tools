ae27dd4da0b1f004a8c8b4f76411bedd9ffcebf2
Oded Arbel <oded@geek.co.il>
2016-01-02 04:00:22 +0200

    record uploader, give a description

--- application/classes/Model/Medium.php
+++ application/classes/Model/Medium.php
@@ -5,7 +5,8 @@ class Model_Medium extends ORM {
 	protected $_table_name = 'media';
 	
 	protected $_belongs_to = [
-			'event' => []
+			'event' => [],
+			'user' => [], // the person that uploaded the content, not necessarily the event owner
 	];
 	
 	protected $_columns = [
@@ -13,6 +14,7 @@ class Model_Medium extends ORM {
 			// foreign keys
 			'event_id' => [],
 			// data fields
+			'title' => [],
 			'filename' => [],
 			// "content_type" is the valid MIME type for S3 stored files,
 			// "application/x-<service>" for external references, such as application/x-youtube
