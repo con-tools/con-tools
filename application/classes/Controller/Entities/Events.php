8ebdb96caa7e034090b4fee53e76574174f4e3ba
Oded Arbel <oded@geek.co.il>
2016-03-13 09:20:20 +0200

    initial events controller

--- application/classes/Controller/Entities/Events.php
+++ application/classes/Controller/Entities/Events.php
@@ -2,26 +2,19 @@
 class Controller_Entities_Events extends Api_Rest_Controller {
 	
 	private function create(Model_Convention $con, Model_User $user, stdClass $data) {
-		// Create model instance
-		$event = new Model_Event();
-		//Add Data
-		$event->user_id = is_null($data->user_id)?$user->pk():$data->user_id;
-		$event->convention = $con;
-		$event->title = $data->title;
-		$event->teaser = $data->teaser;
-		$event->description = $data->description;
-		$event->duration = $data->duration;
-		$event->min_attendees = $data->min_attendees;
-		$event->max_attendees = $data->max_attendees;
-		$event->notes_to_staff = $data->notes_to_staff;
-		$event->notes_to_attendees = $data->notes_to_attendees;
-		$event->scheduling_constraints = $data->scheduling_constraints;
-		//Add data arrays
-		$event->event_tags = $data->event_tags;
-		$event->tags = $data->tags;
-		$event->media = $data->media;
-		//Create the event
-		$event->save();
-		return $event;
+		$tag_event_type = Model_Event_Tag_Type::generate('event_type');
+		$value_event_type = Model_Event_Tag_Value::generate($tag_event_type, $data->event_type);
+		
+		$tag_age_requirement = Model_Event_Tag_Type::generate('age_requirement');
+		$value_age_requirement = Model_Event_Tag_Value::generate($tag_age_requirement, $data->{'age-requirement'});
+		
+		try {
+			return Model_Event::persist($con, $user, $data->title, $value_event_type, $data->teaser, $data->description, 
+					$data->{'requires-registration'}, $data->duration, $data->{'min-attendees'},
+					$data->{'max-attendees'}, $data->{'notes-to-staff'}, $data->{'logistical-requirements'},
+					$data->{'notes-to-attendees'}, $data->{'scheduling-constraints'});
+		} catch (Exception $e) {
+			throw $e;
+		}
 	}
 }
