f52c83c597be255d4fcbc2ebd8f729624e51fa4c
Oded Arbel <oded@geek.co.il>
2016-03-22 01:51:13 +0200

    implement event updates

--- application/classes/Controller/Entities/Events.php
+++ application/classes/Controller/Entities/Events.php
@@ -27,6 +27,8 @@ class Controller_Entities_Events extends Api_Rest_Controller {
 		$o = new Model_Event($id);
 		if (!$o->loaded())
 			throw new Model_Exception_NotFound();
+		if (!$o->convention_id != $this->convention->pk())
+			throw new Api_Exception_Unauthorized($this); // can't hack around convention keys
 		if ($o->isPublic())
 			return $o->for_json();
 		if (!is_null($this->user) and ($this->convention->isManager($this->user) or $o->user_id == $this->user->pk))
@@ -34,7 +36,42 @@ class Controller_Entities_Events extends Api_Rest_Controller {
 		return null;
 	}
 	
-	protected function update($id) {}
+	protected function update($id) {
+		$data = $this->input();
+		if (is_null($this->user))
+			throw new Api_Exception_Unauthorized($this);
+		$o = new Model_Event($id);
+		if (!$o->convention_id != $this->convention->pk())
+			throw new Api_Exception_Unauthorized($this); // can't hack around convention keys
+		if ($this->convention->isManager($this->user)) { // allow to change all fields
+			$o->update(new Validation($data->getFields([
+					'title', 'teaser', 'description', 'duration', 'min_attendees', 'max_attendees',
+					'notes_to_staff', 'logistical_requirements', 'notes_to_attendees', 'scheduling_constraints', 'data',
+					'status', 'price', 'requires_registration'
+			])));
+			if ($data->staff_contact) { // load staff contact
+				$o->staff_contact = Model_User::byEmail($data->staff_contact);
+			}
+			$o->save();
+			if ($data->tags) {
+				foreach ($data->tags as $key => $value) {
+					$type = Model_Event_Tag_Type::generate($this->convention, $key);
+					$o->tag(Model_Event_Tag_Value::generate($type, $value));
+				}
+			}
+			return $o->as_array();
+		}
+		
+		if ($o->user_id == $this->user->pk() and $o->status == Model_Event::STATUS_SUBMITTED) { // owner can update some fields
+			foreach ($data->getFields([
+					'title', 'teaser', 'description', 'duration', 'min_attendees', 'max_attendees',
+					'notes_to_staff', 'logistical_requirements', 'notes_to_attendees', 'scheduling_constraints', 'data'
+			]) as $field => $value)
+				$o->set($field, $value);
+			return $o->save()->for_json();
+		}
+		return null;
+	}
 	
 	protected function delete($id) {
 		if (is_null($this->user) or !$this->convention->isManager($this->user))
@@ -42,6 +79,8 @@ class Controller_Entities_Events extends Api_Rest_Controller {
 		$o = new Model_Event($id);
 		if (!$o->loaded())
 			throw new Model_Exception_NotFound();
+		if (!$o->convention_id != $this->convention->pk())
+			throw new Api_Exception_Unauthorized($this); // can't hack around convention keys
 		$o->cancel();
 	}
 
