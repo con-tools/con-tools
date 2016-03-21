901fb3a206ac83193ef3d3c8aa627df7d97a2382
Oded Arbel <oded@geek.co.il>
2016-03-21 23:11:04 +0200

    handle conditions for showing events

--- application/classes/Controller/Entities/Events.php
+++ application/classes/Controller/Entities/Events.php
@@ -1,20 +1,39 @@
 <?php
 class Controller_Entities_Events extends Api_Rest_Controller {
 	
-	private function create(Model_Convention $con, Model_User $user, stdClass $data) {
-		$tag_event_type = Model_Event_Tag_Type::generate('event_type');
-		$value_event_type = Model_Event_Tag_Value::generate($tag_event_type, $data->event_type);
+	protected function create(Model_Convention $con, Model_User $user) {
+		if (is_null($this->user)) throw new Api_Exception_Unauthorized();
+		$data = $this->input();
+		$tag_event_type = Model_Event_Tag_Type::generate($con, 'event_type');
+		$event_type = Model_Event_Tag_Value::generate($tag_event_type, $data->event_type);
 		
-		$tag_age_requirement = Model_Event_Tag_Type::generate('age_requirement');
-		$value_age_requirement = Model_Event_Tag_Value::generate($tag_age_requirement, $data->{'age-requirement'});
+		$tag_age_requirement = Model_Event_Tag_Type::generate($con, 'age_requirement');
+		$age_requirement = Model_Event_Tag_Value::generate($tag_age_requirement, $data->age_requirement);
 		
 		try {
-			return Model_Event::persist($con, $user, $data->title, $value_event_type, $data->teaser, $data->description, 
-					$data->{'requires-registration'}, $data->duration, $data->{'min-attendees'},
-					$data->{'max-attendees'}, $data->{'notes-to-staff'}, $data->{'logistical-requirements'},
-					$data->{'notes-to-attendees'}, $data->{'scheduling-constraints'});
+			$ev = Model_Event::persist($con, $user, $data->title, $data->teaser, $data->description, 
+					$data->requires_registration, $data->duration, $data->min_attendees,
+					$data->max_attendees, $data->notes_to_staff, $data->logistical_requirements,
+					$data->notes_to_attendees, $data->scheduling_constraints, $data->data);
+			$ev->tag($event_type);
+			$ev->tag($age_requirement);
+			return $ev;
 		} catch (Exception $e) {
 			throw $e;
 		}
 	}
+	
+	protected function retrieve(Model_Convention $con, Model_User $user, $id) {
+		$o = new Model_Event($id);
+		if (!$o->loaded())
+			throw new Model_Exception_NotFound();
+		if ($o->isPublic())
+			return $o->for_json();
+		if (!is_null($this->user) and ($this->convention->isManager($this->user) or $o->user_id == $this->user->pk))
+			return $o->for_json();
+		return null;
+	}
+	
+	protected function update(Model_Convention $con, Model_User $user, $id) {}
+	protected function delete(Model_Convention $con, Model_User $user, $id) {}
 }
