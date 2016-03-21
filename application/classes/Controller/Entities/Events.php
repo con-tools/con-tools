61784915e20cf9df357484968279743dfdf94fca
Oded Arbel <oded@geek.co.il>
2016-03-22 01:59:47 +0200

    admins get full access to all fields

--- application/classes/Controller/Entities/Events.php
+++ application/classes/Controller/Entities/Events.php
@@ -88,7 +88,7 @@ class Controller_Entities_Events extends Api_Rest_Controller {
 		if (!is_null($this->user)) {
 			if ($this->convention->isManager($this->user)) // admins have full access
 				return array_map(function($event){
-					return $event->for_json();
+					return $event->as_array();
 				}, $this->convention->events->find_all());
 			else // return public and owned events
 				return array_map(function($event){ 
