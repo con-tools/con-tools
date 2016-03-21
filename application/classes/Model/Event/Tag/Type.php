9605ee54959b31bee87cf0f34daa60937cc04f41
Oded Arbel <oded@geek.co.il>
2016-03-21 19:30:31 +0200

    initial implementation of event tag type

--- application/classes/Model/Event/Tag/Type.php
+++ application/classes/Model/Event/Tag/Type.php
@@ -19,4 +19,26 @@ class Model_Event_Tag_Type extends ORM {
 			'requirement' => [],
 			'visible' => [ 'type' => 'boolean' ],
 	];
+	
+	/**
+	 * Locate the specified event tag type or generate a new one
+	 * @param Model_Convention $con Convention to which this type belongs
+	 * @param string $title tag type 
+	 */
+	public static function generate(Model_Convention $con, string $title) : Model_Event_Tag_Type {
+		$o = $con->event_tag_types->where('title','=', $title)->find();
+		if ($o->loaded())
+			return $o;
+		return self::persist($con, $title);
+	}
+	
+	public static function persist(Model_Convention $con, string $title) : Model_Event_Tag_Type {
+		$o = new Model_Event_Tag_Type();
+		$o->convention = $con;
+		$o->title = $title;
+		$o->requirement = true;
+		$o->visible = true;
+		$o->save();
+		return $o;
+	}
 }
