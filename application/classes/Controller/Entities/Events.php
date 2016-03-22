60fe063b61ef29719aeac899b1e071aa08e9717d
Oded Arbel <oded@geek.co.il>
2016-03-22 12:11:35 +0200

    Handle tags add and update (still need to support removal of tags)

--- application/classes/Controller/Entities/Events.php
+++ application/classes/Controller/Entities/Events.php
@@ -1,34 +1,52 @@
 <?php
 class Controller_Entities_Events extends Api_Rest_Controller {
 	
-	protected function create(Model_Convention $con, Model_User $user) {
-		if (is_null($this->user)) throw new Api_Exception_Unauthorized($this);
-		$data = $this->input();
-		$tag_event_type = Model_Event_Tag_Type::generate($con, 'event_type');
-		$event_type = Model_Event_Tag_Value::generate($tag_event_type, $data->event_type);
+	private function generateTags($tagspec) {
+		if (!is_array($tagspec)) // no array, no fun
+			return [];
 		
-		$tag_age_requirement = Model_Event_Tag_Type::generate($con, 'age_requirement');
-		$age_requirement = Model_Event_Tag_Value::generate($tag_age_requirement, $data->age_requirement);
+		$tags = [];
+		foreach ($tagspec as $key => $values) {
+			$tag_type = Model_Event_Tag_Type::generate($this->convention, $key, true, is_array($values));
+			// check for consistency
+			if ($tag_type->requiredOne() and is_array($values))
+				throw new Api_Exception_InvalidInput("Tag '$key' does not support multiple values!");
+			if (!is_array($values))
+				$values = [ $values ];
+			foreach ($values as $value) {
+				$tags[] = Model_Event_Tag_Value::generate($tag_type, $value);
+			}
+		}
+		return $tags;
+	}
+	
+	protected function create() {
+		if (is_null($this->user)) throw new Api_Exception_Unauthorized($this, "Must be logged in!");
+		$data = $this->input();
 		
 		try {
-			$ev = Model_Event::persist($con, $user, $data->title, $data->teaser, $data->description, 
+			// try to figure out tags ahead of generating the event - if the user messed up the tag spec, we
+			// should not create the event
+			$typed_tags = $this->generateTags($data-tags);
+			
+			$ev = Model_Event::persist($this->convention, $this->user, $data->title, $data->teaser, $data->description, 
 					$data->requires_registration, $data->duration, $data->min_attendees,
 					$data->max_attendees, $data->notes_to_staff, $data->logistical_requirements,
 					$data->notes_to_attendees, $data->scheduling_constraints, $data->data);
-			$ev->tag($event_type);
-			$ev->tag($age_requirement);
+			foreach ($typed_tags as $tag)
+				$ev->tag($tag);
 			return $ev;
 		} catch (Exception $e) {
 			throw $e;
 		}
 	}
 	
-	protected function retrieve(Model_Convention $con, Model_User $user, $id) {
+	protected function retrieve($id) {
 		$o = new Model_Event($id);
 		if (!$o->loaded())
 			throw new Model_Exception_NotFound();
-		if (!$o->convention_id != $this->convention->pk())
-			throw new Api_Exception_Unauthorized($this); // can't hack around convention keys
+		if ($o->convention_id != $this->convention->pk())
+			throw new Api_Exception_Unauthorized($this, "Incorrect convention selected for event {$o->pk()} ({$o->convention->pk()} != {$o->convention_id})"); // can't hack around convention keys
 		if ($o->isPublic())
 			return $o->for_json();
 		if (!is_null($this->user) and ($this->convention->isManager($this->user) or $o->user_id == $this->user->pk))
@@ -39,10 +57,10 @@ class Controller_Entities_Events extends Api_Rest_Controller {
 	protected function update($id) {
 		$data = $this->input();
 		if (is_null($this->user))
-			throw new Api_Exception_Unauthorized($this);
+			throw new Api_Exception_Unauthorized($this, "Must be logged in!");
 		$o = new Model_Event($id);
 		if (!$o->convention_id != $this->convention->pk())
-			throw new Api_Exception_Unauthorized($this); // can't hack around convention keys
+			throw new Api_Exception_Unauthorized($this, "Incorrect convention selected!"); // can't hack around convention keys
 		if ($this->convention->isManager($this->user)) { // allow to change all fields
 			$o->update(new Validation($data->getFields([
 					'title', 'teaser', 'description', 'duration', 'min_attendees', 'max_attendees',
@@ -54,12 +72,11 @@ class Controller_Entities_Events extends Api_Rest_Controller {
 			}
 			$o->save();
 			if ($data->tags) {
-				foreach ($data->tags as $key => $value) {
-					$type = Model_Event_Tag_Type::generate($this->convention, $key);
-					$o->tag(Model_Event_Tag_Value::generate($type, $value));
+				foreach ($this->generateTags($data->tags) as $tag) {
+					$o->tag($tag);
 				}
 			}
-			return $o->as_array();
+			return $o->for_json();
 		}
 		
 		if ($o->user_id == $this->user->pk() and $o->status == Model_Event::STATUS_SUBMITTED) { // owner can update some fields
@@ -75,12 +92,12 @@ class Controller_Entities_Events extends Api_Rest_Controller {
 	
 	protected function delete($id) {
 		if (is_null($this->user) or !$this->convention->isManager($this->user))
-			throw new Api_Exception_Unauthorized($this);
+			throw new Api_Exception_Unauthorized($this, "Not authorized to cancel events!");
 		$o = new Model_Event($id);
 		if (!$o->loaded())
 			throw new Model_Exception_NotFound();
 		if (!$o->convention_id != $this->convention->pk())
-			throw new Api_Exception_Unauthorized($this); // can't hack around convention keys
+			throw new Api_Exception_Unauthorized($this, "Incorrect convention selected!"); // can't hack around convention keys
 		$o->cancel();
 	}
 
@@ -88,7 +105,7 @@ class Controller_Entities_Events extends Api_Rest_Controller {
 		if (!is_null($this->user)) {
 			if ($this->convention->isManager($this->user)) // admins have full access
 				return array_map(function($event){
-					return $event->as_array();
+					return $event->for_json();
 				}, $this->convention->events->find_all());
 			else // return public and owned events
 				return array_map(function($event){ 
