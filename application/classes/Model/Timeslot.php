d4028a64db04e3fb806292e2404853b72fded582
Oded Arbel <oded@geek.co.il>
2016-03-22 23:57:29 +0200

    fixed hosts lookup; fancy for_json

--- application/classes/Model/Timeslot.php
+++ application/classes/Model/Timeslot.php
@@ -7,8 +7,8 @@ class Model_Timeslot extends ORM {
 	];
 	
 	protected $_has_many = [
-			'hosts' => [ 'model' => 'user', 'through' => 'timeslot_hosts' ],
-			'locations' => [ 'model' => 'location', 'through' => 'timeslot_locations' ],
+			'hosts' => [ 'model' => 'user', 'through' => 'timeslot_hosts', 'far_key' => 'user_id' ],
+			'locations' => [ 'model' => 'Location', 'through' => 'timeslot_locations' ],
 			'tickets' => [],
 	];
 	
@@ -23,4 +23,29 @@ class Model_Timeslot extends ORM {
 			'max_attendees' => [],
 			'notes_to_attendees' => [],
 	];
+	
+	public static function persist(Model_Event $event, DateTime $start, $duration, $min_attendees, $max_attendees, 
+			$notes_to_attendees) : Model_Timeslot {
+		$o = new Model_Timeslot();
+		$o->event = $event;
+		$o->start_time = $start;
+		$o->duration = $duration ?: $event->duration;
+		$o->min_attendees = $min_attendees ?: $event->min_attendees;
+		$o->max_attendees = $max_attendees ?: $event->max_attendees; 
+		$o->notes_to_attendees = $notes_to_attendees ?: $event->notes_to_attendees;
+		return $o->save();
+	}
+	
+	public function for_json() {
+		return array_merge(array_filter(parent::for_json(),function($key){
+			return in_array($key, [
+					'id', 'duration', 'min-attendees', 'max-attendees', 'notes-to-attendees'
+			]);
+		},ARRAY_FILTER_USE_KEY),[
+				'event' => $this->event->for_json(),
+				'start' => $this->start_time->format(DateTime::ATOM),
+				'locations' => self::result_for_json($this->locations->find_all()),
+				'hosts' => self::result_for_json($this->hosts->find_all()),
+		]);
+	}
 }
