849b3fe0aabac0be0e62270a5f5a3c0aeee53d21
Oded Arbel <oded@geek.co.il>
2016-03-24 13:52:23 +0200

    add created and updated timestamps to events

--- database/schema/schema-4.sql
+++ database/schema/schema-4.sql
@@ -1,2 +1,4 @@
+ALTER TABLE `events` ADD COLUMN `created_time` DATETIME DEFAULT CURRENT_TIMESTAMP AFTER `description`;
+ALTER TABLE `events` ADD COLUMN `updated_time` TIMESTAMP AFTER `created_time`;
 
 UPDATE `system_settings` SET `value` = '4' WHERE `name` = 'data-version' and `id` > 0;
