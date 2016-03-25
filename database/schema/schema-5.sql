602290ea79a3267caf6b3d50a06eaa46736e9022
Oded Arbel <oded@geek.co.il>
2016-03-25 19:28:10 +0300

    prevent multiple tag values instances for the same event

--- database/schema/schema-5.sql
+++ database/schema/schema-5.sql
@@ -1,2 +1,4 @@
+ALTER TABLE `event_tags` ADD UNIQUE INDEX `event_tags_uk_1` (`event_id`,`event_tag_value_id`)  COMMENT '';
+
 
 UPDATE `system_settings` SET `value` = '5' WHERE `name` = 'data-version' and `id` > 0;
