6c887a914f9cc33df8e05512ffd7bb9a18ddb341
Oded Arbel <oded@geek.co.il>
2016-03-26 12:50:07 +0300

    programmatic mysql inits load last dump and any newer schemas

--- database/docker-init.sh
+++ database/docker-init.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 mysql -uroot -p$MYSQL_ROOT_PASSWORD -e 'CREATE DATABASE `controll`;'
-for file in /docker-entrypoint-initdb-sh.d/*; do
-	echo "Importing $file..."
-	mysql -uroot -p$MYSQL_ROOT_PASSWORD controll < $file
-	echo "Done"
+# find last dump
+dump="$(ls /dumps/dump* | sort | tail -n 1)"
+echo "Initializing with database dump $dump"
+mysql -uroot -p$MYSQL_ROOT_PASSWORD controll < $dump
+# apply any missing schemas
+index="$(perl -nle 'm/dump-(\d+)/ and print $1' <<<"$dump")"
+while true; do
+    index=$(( $index + 1 ))
+    if [ -f /schema/schema-$index.sql ]; then
+	echo "Updating database with /schema/schema-$index.sql"
+	mysql -uroot -p$MYSQL_ROOT_PASSWORD controll < /schema/schema-$index.sql
+    else
+	break
+    fi
 done
