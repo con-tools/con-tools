8f94543097e99f0fffc523056a69c0b8ffd03560
Oded Arbel <oded@geek.co.il>
2016-03-27 00:54:59 +0300

    remove extranous logging

--- application/classes/Model/Timeslot.php
+++ application/classes/Model/Timeslot.php
@@ -60,8 +60,6 @@ class Model_Timeslot extends ORM {
 	 */
 	public function conflicts(DateTime $start, DateTime $end) {
 		$beforeend = (clone $end)->sub(new DateInterval("PT1S"));
-		error_log("Comparing timeslot " . $this->start_time->getTimestamp() . "-" . $this->end_time->getTimestamp() .
-				" and " . $start->getTimestamp() . "-" . $end->getTimestamp());
 		return !(
 				($this->end_time->diff($start)->invert == 0) // my end is <= than their start
 				or
