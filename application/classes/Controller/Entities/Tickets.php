f0a404935153f456309170dcba96d14d56f75225
Oded Arbel <oded@geek.co.il>
2016-03-30 00:28:42 +0300

    implement missing methods, except update (TBD)

--- application/classes/Controller/Entities/Tickets.php
+++ application/classes/Controller/Entities/Tickets.php
@@ -1,6 +1,6 @@
 <?php
 
-class Controller_Entities_Ticketse extends Api_Rest_Controller {
+class Controller_Entities_Tickets extends Api_Rest_Controller {
 	
 	public function create() {
 		if (!$this->user)
@@ -26,6 +26,54 @@ class Controller_Entities_Ticketse extends Api_Rest_Controller {
 			throw new Api_Exception_InvalidInput($this, "Not enough tickets left");
 		}
 		return $ticket->for_json();
+	}
+	
+	public function retrieve($id) {
+		$ticket = new Model_Ticket($id);
+		if ($ticket->loaded() && ($ticket->user == $this->user || $this->systemAccessAllowed()))
+			return $ticket->for_json();
+		throw new Api_Exception_InvalidInput($this, "No valid tickets found to display");
+	}
+	
+	public function update($id) {
+		// TODO: do stuff
+	}
+	
+	public function delete($id) {
+		if (!$this->systemAccessAllowed())
+			throw new Api_Exception_Unauthorized($this, "Not authorized to delete tickets");
+		$ticket = new Model_Ticket($id);
+		if ($ticket->loaded())
+			throw new Api_Exception_InvalidInput($this, "No ticket found");
+		if ($ticket->isAuthorized())
+			throw new Api_Exception_InvalidInput($this, "Can't delete authorized tickets, cancel it first");
+		$ticket->delete();
+	}
+	
+	public function catalog() {
+		$data = $this->input();
+		// two different base modes - user and admin/convention
+		$filters = [];
+		if ($this->systemAccessAllowed()) {
+			// ehmm.. no default filters, unless the caller asked for a user filter
+			if ($data->by_user) {
+				if (is_numeric($data->by_user))
+					$filters['user_id'] = $data->by_user;
+				else
+					$filters['email'] = $data->by_user;
+			}
+		} else {
+			// verify user and filter by them
+			if (!$this->user)
+				throw new Api_Exception_Unauthorized($this, "You must be logged in to see your tickets");
+			$filters['user_id'] = $this->user->pk();
+		}
+		
+		if ($data->by_event)
+			$filters['event_id'] = $data->by_event;
+		if ($data->by_timeslot)
+			$filters['timeslot_id'] = $data->by_timeslot;
+		return ORM::result_for_json($this->convention->getTickets($filters), 'for_json');
 	}
 	
 }
