3552242f3de356eccdb59a6317f25e583c9be5b5
Oded Arbel <oded@geek.co.il>
2016-03-30 15:07:56 +0300

    better API for payment processor

--- application/classes/Payment/Processor.php
+++ application/classes/Payment/Processor.php
@@ -19,4 +19,15 @@ class Payment_Processor {
 		}
 	}
 	
+	/**
+	 * Generate HTML for a transaction, that is used to redirect the client to
+	 * the payment processor transaction fulfillment page.
+	 *
+	 * This method accepts a set of tickets and either throws an exception
+	 * if it can't handle them,
+	 *
+	 * @param array $tickets list of Model_Ticket to process
+	 */
+	abstract public function createTransactionHTML(Model_Sale $sale, $okurl, $failurl);
+	
 }
