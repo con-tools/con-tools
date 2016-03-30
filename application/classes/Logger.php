68bdda9b5602c2ad0377e7232684232c711290d3
Oded Arbel <oded@geek.co.il>
2016-03-30 18:17:14 +0300

    dont override Kohana logger, because not nice

--- application/classes/Logger.php
+++ application/classes/Logger.php
@@ -1,24 +1,24 @@
 <?php
 
-class Log extends Kohana_Log {
+class Logger {
 
 	public static function debug($message, array $context = NULL, array $additional = NULL) {
-		return Kohana::$log->add(self::DEBUG, $message, $context, $additional);
+		return Kohana::$log->add(Kohana_Log::DEBUG, $message, $context, $additional);
 	}
 	
 	public static function info($message, array $context = NULL, array $additional = NULL) {
-		return Kohana::$log->add(self::INFO, $message, $context, $additional);
+		return Kohana::$log->add(Kohana_Log::INFO, $message, $context, $additional);
 	}
 	
 	public static function warn($message, array $context = NULL, array $additional = NULL) {
-		return Kohana::$log->add(self::WARNING, $message, $context, $additional);
+		return Kohana::$log->add(Kohana_Log::WARNING, $message, $context, $additional);
 	}
 	
 	public static function error($message, array $context = NULL, array $additional = NULL) {
-		return Kohana::$log->add(self::ERROR, $message, $context, $additional);
+		return Kohana::$log->add(Kohana_Log::ERROR, $message, $context, $additional);
 	}
 	
 	public static function fatal($message, array $context = NULL, array $additional = NULL) {
-		return Kohana::$log->add(self::EMERGENCY, $message, $context, $additional);
+		return Kohana::$log->add(Kohana_Log::EMERGENCY, $message, $context, $additional);
 	}
 }
