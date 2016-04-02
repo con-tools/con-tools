ff41996910d104864fe47486339b7b5f0a1a7b43
Oded Arbel <oded@geek.co.il>
2016-04-02 09:18:09 +0300

    Add organization membership model

--- database/schema/schema-8.sql
+++ database/schema/schema-8.sql
@@ -1,2 +1,12 @@
 
+CREATE TABLE `organization_members` (
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT COMMENT '',
+  `organizer_id` INT UNSIGNED NOT NULL COMMENT '',
+  `user_id` INT UNSIGNED NOT NULL COMMENT '',
+  `membership` VARCHAR(50) NOT NULL COMMENT 'organization membership code/number/registration etc.',
+  PRIMARY KEY (`id`) COMMENT '',
+  FOREIGN KEY `fk_organizer_id` (`organizer_id`) REFERENCES `organizers` (`id`) ON DELETE RESTRICT,
+  FOREIGN KEY `fk_user_id` (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
+) ENGINE=MyISAM DEFAULT CHARACTER SET UTF8;
+
 UPDATE `system_settings` SET `value` = '8' WHERE `name` = 'data-version' and `id` > 0;
