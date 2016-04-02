f15ce56714dcc6534a58b899f69ff2a3d89514b4
Oded Arbel <oded@geek.co.il>
2016-04-02 20:10:34 +0300

    basic coupon type implementation

---
+++ application/classes/Controller/Entities/Coupontypes.php
@@ -0,0 +1,53 @@
+<?php
+
+class Controller_Entities_Coupontypes extends Api_Rest_Controller {
+
+	public function create() {
+		if (!$this->systemAccessAllowed())
+			throw new Api_Exception_Unauthorized($this, "Not allowed to list coupons");
+		$data = $this->input();
+		if (!$data->title)
+			throw new Api_Exception_InvalidInput($this, "Mandatory field 'title' is missing");
+		if (!$data->type)
+			throw new Api_Exception_InvalidInput($this, "Madatory field 'type' is missing");
+		if (!$data->amount)
+			throw new Api_Exception_InvalidInput($this, "Mandatory field 'amount' is missing");
+		if (!$data->category)
+			throw new Api_Exception_InvalidInput($this, "Mandatory field 'category' is missing");
+		if (!$data->multiuse || !is_bool($data->multiuse))
+			throw new Api_Exception_InvalidInput($this, "Mandatory boolean field 'multiuse' is missing or invalid");
+		return Model_Coupon_Type::persist($this->convention, $data->title, stristr($data->type, 'fixed')?true:false,
+				$data->amount, $data->category, $data->multiuse, $data->code)->for_json();
+	}
+
+	public function retrieve($id) {
+		if (!$this->systemAccessAllowed())
+			throw new Api_Exception_Unauthorized($this, "Not allowed to list coupons");
+		$coupontype = new Model_Coupon_Type($id);
+		if ($coupontype->loaded())
+			return $coupontype->for_json();
+		throw new Api_Exception_InvalidInput($this, "Failed to find coupong type $id");
+	}
+
+	public function update($id) {
+		if (!$this->systemAccessAllowed())
+			throw new Api_Exception_Unauthorized($this, "Not allowed to list coupons");
+		throw new Api_Exception_Unimplemented($this);
+	}
+
+	public function delete($id) {
+		if (!$this->systemAccessAllowed())
+			throw new Api_Exception_Unauthorized($this, "Not allowed to list coupons");
+		$coupontype = new Model_Coupon_Type($id);
+		if ($coupontype->loaded())
+			$coupontype->delete();
+		return true;
+	}
+
+	public function catalog() {
+		if (!$this->systemAccessAllowed())
+			throw new Api_Exception_Unauthorized($this, "Not allowed to list coupons");
+		return ORM::result_for_json(Model_Coupon_Type::byConvention($this->convention));
+	}
+
+}
--- application/classes/Model/Coupon/Type.php
+++ application/classes/Model/Coupon/Type.php
@@ -19,4 +19,30 @@ class Model_Coupon_Type extends ORM {
 			'code' => [],
 	];
 	
+	public static function Model_persist(Model_Convention $con, string $title, $fixed_discount, $amount,
+				string $category, $multiuse, $code = null) : Model_Coupon_Type {
+		$o = new Model_Coupon_Type();
+		$o->convention = $con;
+		$o->title = $title;
+		$o->discount_type = $fixed_discount ? 'fixed' : 'percent';
+		$o->amount = $amount;
+		$o->category = $category;
+		$o->multiuse = $multiuse;
+		$o->code = $code;
+		$o->save();
+		return $o;
+	}
+
+	public static function byConvention(Model_Convention $con) : Database_Result {
+		return (new Model_Coupon_Type())->where('convention_id','=',$con->pk())->find_all();
+	}
+	
+	public function for_json() {
+		return array_merge(array_filter(parent::for_json(),function($key){
+			return $key != 'convention_id';
+		},ARRAY_FILTER_USE_KEY), [
+				'convention' => $this->convention->for_json(),
+		]);
+	}
+	
 };
