8aeff3efc7bb03911483808ac0d7ec2b86bc72d9
Oded Arbel <oded@geek.co.il>
2016-04-09 15:05:14 +0300

    manage SKUs

---
+++ application/classes/Controller/Entities/Merchandiseskus.php
@@ -0,0 +1,55 @@
+<?php
+
+class Controller_Entities_MerchandiseSKUs extends Api_Rest_Controller {
+	
+	public function create() {
+		if (!$this->systemAccessAllowed())
+			throw new Api_Exception_Unauthorized($this, "Not allowed to create merchandise SKUs");
+		$data = $this->input();
+		if (!($title = $data->title))
+			throw new Api_Exception_InvalidInput($this, "Mandatory parameter title missing");
+		if (!($price = $data->price))
+			throw new Api_Exception_InvalidInput($this, "Mandatory parameter price missing");
+		$description = $data->description;
+		return Model_Merchandise_Sku::persist($this->convention, $title, $price, $description)->for_json();
+	}
+	
+	public function retrieve($id) {
+		$sku = new Model_Merchandise_Sku($id);
+		if ($sku->loaded())
+			return $sku->for_json();
+		throw new Api_Exception_Notfound($this);
+	}
+	
+	public function update($id) {
+		if (!$this->systemAccessAllowed())
+			throw new Api_Exception_Unauthorized($this, "Not allowed to delete merchandise SKUs");
+		$sku = new Model_Merchandise_Sku($id);
+		if (!$sku->loaded())
+			throw new Api_Exception_Notfound($this);
+		$data = $this->input();
+		$title = $data->title;
+		$price = $data->price;
+		$description = $data->description;
+		if ($title)
+			$sku->title = $title;
+		if ($description)
+			$sku->description = $description;
+		if ($price)
+			$sku->price = $price;
+		return $sku->save()->for_json();
+	}
+	
+	public function delete($id) {
+		if (!$this->systemAccessAllowed())
+			throw new Api_Exception_Unauthorized($this, "Not allowed to delete merchandise SKUs");
+		$sku = new Model_Merchandise_Sku($id);
+		if ($sku->loaded())
+			$sku->delete();
+		return true;
+	}
+	
+	public function catalog() {
+		return ORM::result_for_json(Model_Merchandise_Sku::queryForConvention($this->convention)->find_all());
+	}
+}
---
+++ application/classes/Model/Merchandise/Sku.php
@@ -0,0 +1,51 @@
+<?php
+
+class Model_Merchandise_Sku extends ORM {
+	
+	protected $_belongs_to = [
+			'convention' => [],
+	];
+	
+	protected $_has_many = [
+			'purchases' => [],
+	];
+	
+	protected $_columns = [
+			'id' => [],
+			// foreign keys
+			'convention_id' => [],
+			// data fields
+			'title' => [],
+			'created_time' => [ 'type' => 'DateTime' ],
+			'description' => [],
+			'price' => [],
+	];
+	
+	public static function persist(Model_Convention $con, string $title, $price, $description = null) : Model_Merchandise_Sku {
+		$o = new Model_Merchandise_Sku();
+		$o->convention = $con;
+		$o->created_time = new DateTime();
+		$o->title = $title;
+		$o->description = $description;
+		$o->price = $price;
+		return $o->save();
+	}
+
+	/**
+	 * Generate a query for all merchandise belonging to a convention
+	 * @param Model_Convention $con convention to list for
+	 * @return ORM a model object with the query loaded
+	 */
+	public static function queryForConvention(Model_Convention $con) : ORM {
+		$query = (new Model_Merchandise_Sku())->where('convention_id', '=', $con->pk());
+		return $query;
+	}
+	
+	public function for_json() {
+		return array_merge(array_filter(parent::for_json(),function($key){
+			return !in_array($key, ['convention-id']);
+		},ARRAY_FILTER_USE_KEY),[
+				'convention' => $this->convention->for_json(),
+		]);
+	}
+}
