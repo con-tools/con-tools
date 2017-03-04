447cb35ed19c54e7b06a1fb0f9f9b7447c8b7517
Oded Arbel <oded@geek.co.il>
2017-03-04 21:19:06 +0200

    handle edge case of 0 interval

--- application/classes/ORM.php
+++ application/classes/ORM.php
@@ -75,9 +75,12 @@ class ORM extends Kohana_ORM {
 		list($date,$time) = explode("T",$interval->format("P%yY%mM%dDT%hH%iM%sS"));
 		// now, we need to remove anything that is a zero, but make sure to not remove
 		// something like 10D or 20D
-		return
+		$res =
 			str_replace([ 'M0D', 'Y0M', 'P0Y' ], [ 'M', 'Y', 'P' ], $date) .
 			rtrim(str_replace([ 'M0S', 'H0M', 'T0H'], [ 'M', 'H', 'T' ], "T$time"),"T");
+		if ($res == 'P') // edge case - if we remove everything, DateInterval will hate us later
+			return 'PT0S';
+		return $res;
 	}
 	
 	public function compile() {
