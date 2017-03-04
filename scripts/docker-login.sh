274417dadfd33ab8553c4f7e9c7a2e4434df9288
Oded Arbel <oded@geek.co.il>
2017-03-04 14:51:27 +0200

    use docker CLI to get IP address for docker mysql

--- scripts/docker-login.sh
+++ scripts/docker-login.sh
@@ -4,6 +4,7 @@ email="admin@con-troll.org"
 name="Admin User"
 password="123"
 endpoint="http://localhost:8080"
+mysql_ip="$(docker inspect --format='{{.NetworkSettings.IPAddress}}' controll_mysql_1)"
 
 function try_login() {
     curl -sf "${endpoint}/auth/signin" -H 'Content-Type: application/json' -d "{\"email\":\"${email}\",\"password\":\"${password}\"}" > >(jq -r .token)
@@ -16,4 +17,4 @@ function create_user() {
 # try to log in with hard coded values
 try_login || ( create_user && try_login ) >/dev/null
 
-mysql -uroot -psecret -h172.17.0.2 controll < $(dirname $0)/../database/dumps/docker-add-admin-to-convention.sql >/dev/null 2> >(fgrep -v 'Using a password on the command line')
+mysql -uroot -psecret -h$mysql_ip controll < $(dirname $0)/../database/dumps/docker-add-admin-to-convention.sql >/dev/null 2> >(fgrep -v 'Using a password on the command line')
